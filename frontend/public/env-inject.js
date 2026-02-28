abe524a512bd305ca303d6d89058c8199e12cd22
Dennis Siemensma <github@dennissiemensma.nl>
2026-02-28 13:52:55 +0100

    Simplify backend URL injection logic and optimize dashboard data timer calculation

--- frontend/public/env-inject.js
+++ frontend/public/env-inject.js
@@ -1,11 +1,7 @@
 // This script injects the BACKEND_URL from the environment into the global window object at runtime.
 (function() {
-  var backendUrl = (function() {
-    // Try to read from injected environment variable (nginx or Docker)
-    return (window.BACKEND_URL_ENV || process.env.BACKEND_URL || null);
-  })();
+  var backendUrl = window.BACKEND_URL_ENV || null;
   if (backendUrl) {
     window.BACKEND_URL = backendUrl;
   }
 })();
-
